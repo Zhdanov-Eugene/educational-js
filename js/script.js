34862f2b1f5b305d4dd3944d192845a82917f809
EugeneZhdanov <jeka7753191@gmail.com>
2022-11-10 17:27:03 +0200

    task2

--- js/script.js
+++ js/script.js
@@ -64,3 +64,50 @@
 // personalMovieDB.movies[c] = d;
 
 // console.log(personalMovieDB);
+
+/* 2 Задание на урок: */
+// console.log(NaN || 2 || undefined); //2
+
+// console.log(NaN && 2 && undefined); //NaN
+
+// console.log(1 && 2 && 3); //3
+
+// console.log((!1 && 2) || !3); //false
+
+// console.log(25 || (null && !3)); //25
+
+// console.log(NaN || null || !3 || undefined || 5); //5
+
+// console.log(NaN || (null && !3 && undefined) || 5); //5
+
+// console.log((5 === 5 && 3 > 1) || 5); ////true-5
+
+/*Выполняется ли условие?*/
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if ((hamburger === 3 && cola) || (fries === 3 && nuggets)) {
+//   console.log("Done!");
+// } ////2-true
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//   console.log("Done!");
+// } //3
+
+// if ((hamburger && cola) || (fries === 3 && nuggets)) {
+//   console.log("Done!");
+// } //2
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//   console.log("Done!");
+// } //2
+// if ((hamburger && cola) || (fries === 3 && nuggets)) {
+//   console.log("Done!");
+// } //false
