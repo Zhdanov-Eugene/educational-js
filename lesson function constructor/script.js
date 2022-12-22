d9b0f857af47f57947392f1a881859c9cc10cf1a
EugeneZhdanov <jeka7753191@gmail.com>
2022-12-22 17:49:32 +0200

    class

--- lesson function constructor/script.js
+++ lesson function constructor/script.js
@@ -109,3 +109,35 @@ obj1.sayNumber();
 
 const double = (a) => a * 2;
 console.log(double(4));
+//
+//
+//
+//
+//
+//
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+class ColoredReactangleWidthText extends Rectangle {
+  constructor(height, width, text, bgColor) {
+    super(height, width);
+    this.text = text;
+    this.bgColor = bgColor;
+  }
+  showMyProps() {
+    console.log(`Text ${this.text}, color:${this.bgColor}`);
+  }
+}
+const div = new ColoredReactangleWidthText(25, 10, "Hello Boys", "azure");
+div.showMyProps();
+console.log(div.calcArea());
+const square = new Rectangle(10, 10);
+const long = new Rectangle(20, 100);
+console.log(square.calcArea());
+console.log(long.calcArea());
