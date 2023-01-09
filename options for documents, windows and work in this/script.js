f67a68766f489ad08bbfbd0fd41e3e2aec803f31
EugeneZhdanov <jeka7753191@gmail.com>
2023-01-09 16:08:09 +0200

    promise

--- options for documents, windows and work in this/script.js
+++ options for documents, windows and work in this/script.js
@@ -24,3 +24,64 @@ console.log(style.display);
 //
 console.log(document.documentElement.scrollTop);
 //document.documentElement.scrollTop = 0;
+//
+//
+//
+//PROMISE
+//
+// console.log("запрос  данних...");
+
+// const req = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     console.log("подготовка  данних...");
+
+//     const product = {
+//       name: "tv",
+//       price: 2000,
+//     };
+
+//     resolve(product);
+//   }, 2000);
+// });
+
+// req
+//   .then((product) => {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         product.status = "order";
+//         resolve(product);
+//         //reject();
+//         console.log(product);
+//       }, 2000);
+//       console.log("данные получены");
+//     })
+//       .then((data) => {
+//         data.modify = true;
+//         return data;
+//       })
+//       .then((data) => {
+//         console.log(data);
+//       });
+//   })
+//   .catch(() => {
+//     console.log("данные не получены - ерорр");
+//   })
+//   .finally(() => {
+//     console.log("finaly");
+//   });
+//
+//
+const test = (time) => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), time);
+  });
+};
+// test(1000).then(() => console.log("1000 ms"));
+// test(2000).then(() => console.log("2000 ms"));
+Promise.all([test(1000), test(2000)]).then(() => {
+  console.log("all");
+});
+
+Promise.race([test(1000), test(2000)]).then(() => {
+  console.log("all");
+});
