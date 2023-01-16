07de3229057093009ea201abb54c31f70099db4b
EugeneZhdanov <jeka7753191@gmail.com>
2023-01-16 17:15:53 +0200

    get set

--- lesson server and other/js/testscript.js
+++ lesson server and other/js/testscript.js
@@ -98,3 +98,18 @@
 // console.log(str.match(/\W/i));
 // console.log(str.match(/\W/gi));
 // console.log(str.match(/\D/gi));
+//
+//
+// const persone = {
+//   name: "alex",
+//   age: 25,
+
+//   get userAge() {
+//     return this.age;
+//   },
+//   set userAge(num) {
+//     this.age = num;
+//   },
+// };
+// console.log((persone.userAge = 30));
+// console.log(persone.userAge);
