d94e64dc76db38377cbbdd8fc842238f889632cb
EugeneZhdanov <jeka7753191@gmail.com>
2023-01-31 16:30:28 +0200

    function generation

--- js/testscript.js
+++ js/testscript.js
@@ -21,3 +21,41 @@ $(document).ready(function () {
     );
   });
 });
+("use strict");
+// function* generator() {
+//   yield "S";
+//   yield "C";
+//   yield "R";
+//   yield "I";
+//   yield "P";
+//   yield "T";
+// }
+// const str = generator();
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next().value);
+//
+// function* count(n) {
+//   for (let i = 0; i < n; i++) {
+//     yield i;
+//   }
+// }
+// const counter = count(7);
+// console.log(counter.next().value);
+// console.log(counter.next().value);
+// console.log(counter.next().value);
+//
+function* count(n) {
+  for (let i = 0; i < n; i++) {
+    yield i;
+  }
+}
+for (let k of count(7)) {
+  console.log(k);
+}
+//
